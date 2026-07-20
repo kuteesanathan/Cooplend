c450b450e76df6658b05aed8d40bb13c7493d747
kuteesanathan <natiekuteesa@gmail.com>
2026-07-20 16:31:36 +0300

    security package

---
+++ src/main/java/com/tui/cooplend/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.tui.cooplend.security;
+
+import com.tui.cooplend.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("No user with email " + email));
+    }
+}
