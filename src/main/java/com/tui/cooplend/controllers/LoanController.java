2c3092c01e61fae4c5e3ac090227ef285d721009
kuteesanathan <natiekuteesa@gmail.com>
2026-07-24 09:29:35 +0300

    loan controller

--- src/main/java/com/tui/cooplend/controllers/LoanController.java
+++ src/main/java/com/tui/cooplend/controllers/LoanController.java
@@ -1,8 +1,36 @@
 package com.tui.cooplend.controllers;
 
+import com.tui.cooplend.dtos.LoanResponse;
+import com.tui.cooplend.enums.LoanStatus;
 import com.tui.cooplend.repositories.LoanRepository;
+import com.tui.cooplend.services.LoanService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/loans")
+@AllArgsConstructor
 public class LoanController {
-    private final LoanRepository loanRepository;
-    private final LoanAp
+    private final LoanService loanService;
+
+//    @GetMapping
+//    public ResponseEntity<Page<LoanResponse>> search(
+//            @RequestParam(required = false) LoanStatus status,
+//            @RequestParam(required = false) Long memberId,
+//            @RequestParam(required = false) Long productId,
+//            @RequestParam(required = false) BigDecimal minBalance,
+//            @RequestParam(required = false) BigDecimal maxBalance,
+//            Pageable pageable) {
+//        return ResponseEntity.ok(loanService.search(status, memberId, productId, minBalance, maxBalance, pageable));
+//    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LoanResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(loanService.getById(id));
+    }
 }
