b9354f3bfcaa7d8cb2fa7230a4136a619f49c2fd
kuteesanathan <natiekuteesa@gmail.com>
2026-07-30 14:15:30 +0300

    debugging

--- src/main/java/com/tui/cooplend/security/JwtAuthenticationFilter.java
+++ src/main/java/com/tui/cooplend/security/JwtAuthenticationFilter.java
@@ -34,6 +34,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         String token = authHeader.substring(7);
+
         try {
             String username = jwtService.extractUsername(token);
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
