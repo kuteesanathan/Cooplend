1c7085da1a9bcbf596eb1f5b35c92fcacc4828c2
kuteesanathan <natiekuteesa@gmail.com>
2026-08-19 10:23:10 +0300

    minor

--- src/main/java/com/tui/cooplend/config/SecurityConfig.java
+++ src/main/java/com/tui/cooplend/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
         return provider;
 
     }
+    
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
